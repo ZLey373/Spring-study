46aafaf9d27bdc638bc396da15a93c8268f75ddb
ZLey <1690791641@qq.com>
2023-05-25 22:15:43 +0800

    SpringHello

---
+++ Spring-02-HelloSpring/src/test/java/com/zhaolei/Mytest.java
@@ -0,0 +1,13 @@
+package com.zhaolei;
+
+import com.zhaolei.hello.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mytest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Hello hello = (Hello) context.getBean("hello");
+        System.out.printf(hello.toString());
+    }
+}
