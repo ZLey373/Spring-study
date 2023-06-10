cbfff4ddaeb6ed34511d81f5ff57b2f2d862f2e9
ZLey <1690791641@qq.com>
2023-06-10 21:14:33 +0800

    代理的辅助功能

---
+++ Spring-08-proxy/src/main/java/com/zhaolei/demo02/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.zhaolei.demo02;
+
+public class UserServiceImpl implements UserService{
+    public void add() {
+        System.out.println("增");
+    }
+
+    public void delete() {
+        System.out.println("删");
+    }
+
+    public void update() {
+        System.out.println("改");
+    }
+
+    public void query() {
+        System.out.println("查");
+    }
+}
---
+++ Spring-08-proxy/src/main/java/com/zhaolei/demo02/UserServiceProxy.java
@@ -0,0 +1,38 @@
+package com.zhaolei.demo02;
+
+public class UserServiceProxy implements UserService{
+    private UserServiceImpl userService;
+
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void add() {
+        log("add");
+        userService.add();
+    }
+
+    @Override
+    public void delete() {
+        log("delete");
+        userService.delete();
+    }
+
+    @Override
+    public void update() {
+        log("update");
+        userService.update();
+    }
+
+    @Override
+    public void query() {
+        log("query");
+        userService.query();
+    }
+
+    // 扩展一个日志方法
+    public void log(String mesg){
+        System.out.println("使用了"+mesg+"方法");
+    }
+}
