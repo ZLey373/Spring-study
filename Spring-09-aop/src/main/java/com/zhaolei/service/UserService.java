e4a5c6161be2307ca3e18b40cb4ea10351fe170a
ZLey <1690791641@qq.com>
2023-06-23 22:14:22 +0800

    接口以及实体类

---
+++ Spring-09-aop/src/main/java/com/zhaolei/service/UserService.java
@@ -0,0 +1,9 @@
+package com.zhaolei.service;
+
+public interface UserService {
+    public void add();
+    public void delete();
+    public void update();
+    public void select();
+
+}
---
+++ Spring-09-aop/src/main/java/com/zhaolei/service/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zhaolei.service;
+
+public class UserServiceImpl implements UserService{
+    public void add() {
+        System.out.println("增加");
+    }
+
+    public void delete() {
+        System.out.println("删除");
+    }
+
+    public void update() {
+        System.out.println("更新");
+    }
+
+    public void select() {
+        System.out.println("查询");
+
+    }
+}
