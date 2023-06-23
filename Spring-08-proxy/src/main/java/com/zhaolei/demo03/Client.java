313ebdb9453b414792c12bdd0d2fe2b71279dcd1
ZLey <1690791641@qq.com>
2023-06-23 22:12:22 +0800

    动态代理

---
+++ Spring-08-proxy/src/main/java/com/zhaolei/demo03/Client.java
@@ -0,0 +1,26 @@
+package com.zhaolei.demo03;
+
+import com.zhaolei.demo02.UserService;
+import com.zhaolei.demo02.UserServiceImpl;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // 真实角色
+        UserServiceImpl userService = new UserServiceImpl();
+        // 代理角色，不存在
+        ProxyInvocationHandler pih = new ProxyInvocationHandler();
+        // 设置要代理的对象
+        pih.setTarget(userService);
+
+        // 动态生成代理类
+        UserService proxy = (UserService) pih.getProxy();
+        proxy.query();
+
+    }
+
+
+
+
+
+}
